d4454174be4de81c353c642fe16c56b5f77e9d19
DEv0on <bananamen02@gmail.com>
2021-09-18 16:17:47 +0200

    Fixed ability to use flamethrower without permission

---
+++ src/main/kotlin/net/remodded/mixinpatches/mixins/common/CommonPlayerTickHandlerMixin.kt
@@ -0,0 +1,17 @@
+@file:Mixin(CommonPlayerTickHandler::class)
+
+package net.remodded.mixinpatches.mixins.common
+
+import mekanism.common.CommonPlayerTickHandler
+import net.minecraft.entity.player.EntityPlayer
+import net.remodded.reisland.listeners.IslandProtection
+import org.spongepowered.api.entity.living.player.Player
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
+
+@Inject(method = ["isFlamethrowerOn"], at = [At("HEAD")], cancellable = true)
+private fun isFlamethrowerOn(player: EntityPlayer, cir: CallbackInfoReturnable<Boolean>) {
+    if (!IslandProtection.canPlayerInteract(player as Player)) cir.returnValue = false
+}
