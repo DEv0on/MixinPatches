4399430a3b3ed856d9e8d7f549a596f8424f3a6f
DEv0on <bananamen02@gmail.com>
2021-09-18 16:17:30 +0200

    Added event calling for blood lust server booster

---
+++ src/main/kotlin/net/remodded/mixinpatches/mixins/common/BloodAltarMixin.kt
@@ -0,0 +1,30 @@
+package net.remodded.mixinpatches.mixins.common
+
+import WayofTime.bloodmagic.altar.BloodAltar
+import net.minecraftforge.common.MinecraftForge
+import net.remodded.recore.event.AltarFillEvent
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.ModifyVariable
+
+@Mixin(BloodAltar::class)
+class BloodAltarMixin {
+
+    @ModifyVariable(method = ["fillMainTank(I)I"], at = At("HEAD"), ordinal = 0)
+    fun fillMainTank(amount: Int): Int {
+        val event = AltarFillEvent(amount)
+        MinecraftForge.EVENT_BUS.post(event)
+        if (event.isCanceled)
+            return 0
+        return event.addedAmount
+    }
+
+    @ModifyVariable(method = ["sacrificialDaggerCall(IZ)V"], at = At("HEAD"), ordinal = 0)
+    fun sacrificialDaggerCall(amount: Int): Int {
+        val event = AltarFillEvent(amount)
+        MinecraftForge.EVENT_BUS.post(event)
+        if (event.isCanceled)
+            return 0
+        return event.addedAmount
+    }
+}
