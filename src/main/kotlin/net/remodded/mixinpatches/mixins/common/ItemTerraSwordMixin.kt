b5701a1c7f5c2e9175a6178723303941e4b31005
DEv0on <bananamen02@gmail.com>
2021-09-18 16:17:09 +0200

    Fixed bypassing damage protection on swords (Botania) without permission

---
+++ src/main/kotlin/net/remodded/mixinpatches/mixins/common/ItemTerraSwordMixin.kt
@@ -0,0 +1,20 @@
+package net.remodded.mixinpatches.mixins.common
+
+import net.minecraft.entity.player.EntityPlayer
+import net.remodded.reisland.listeners.IslandProtection
+import org.spongepowered.api.entity.living.player.Player
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+import vazkii.botania.common.item.equipment.tool.terrasteel.ItemTerraSword
+
+@Mixin(ItemTerraSword::class, remap = false)
+class ItemTerraSwordMixin {
+
+    @Inject(method = ["trySpawnBurst"], at = [At("HEAD")], cancellable = true)
+    fun trySpawnBurst(player: EntityPlayer, cl: CallbackInfo) {
+        if (!IslandProtection.canPlayerInteract(player as Player))
+            cl.cancel()
+    }
+}
