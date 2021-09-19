2f9c31a13f4f9c74335900a31b9ba567f278925b
DEv0on <bananamen02@gmail.com>
2021-09-19 20:49:55 +0200

    Fixed memory leak in bloodmagic at world unload

---
+++ src/main/kotlin/net/remodded/mixinpatches/mixins/common/GenericHandler.kt
@@ -0,0 +1,24 @@
+@file:Mixin(GenericHandler::class)
+
+package net.remodded.mixinpatches.mixins.common
+
+import WayofTime.bloodmagic.util.handler.event.GenericHandler
+import net.minecraftforge.event.world.WorldEvent
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+
+@Inject(
+    method = ["onWorldUnload"],
+    at = [At(
+        "INVOKE",
+        target = "Ljava/util/Map;remove(Ljava/lang/Object;)Ljava/lang/Object;",
+        ordinal = 3,
+        shift = At.Shift.AFTER
+    )]
+)
+private fun onWorldUnload(event: WorldEvent.Unload, ci: CallbackInfo) {
+    GenericHandler.preventSpawnMap.remove(event.world)
+    GenericHandler.forceSpawnMap.remove(event.world)
+}
